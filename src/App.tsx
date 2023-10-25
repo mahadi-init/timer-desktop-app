3e7b1e0bae40e720a988e60f55ae96bb78568752
mahadi-init <mahadi.init@proton.me>
2023-10-25 12:04:56 +0600

    add time screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,74 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { Button, Input } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { LuTimerReset } from "react-icons/lu";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [value, setValue] = useState(0);
+  const [counter, setCounter] = useState<number>(0);
+  const [timeLeft, setTimeLeft] = useState(0);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  useEffect(() => {
+    if (timeLeft !== 0) {
+      const timeout = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 60000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [timeLeft]);
+
+  const handleEnter = (e: any) => {
+    if (e.key === "Enter") {
+      if (counter <= 0) return;
+
+      setValue(counter);
+      setTimeLeft(counter);
+      setCounter(0);
+    }
+  };
+
+  const handleReset = () => {
+    setTimeLeft(0);
+    setCounter(0);
+    setValue(0);
+  };
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      {value === 0 ? (
+        <p className="mt-20 font-medium mb-1">Countdown is not running</p>
+      ) : (
+        <p className="mt-20 font-medium mb-1">
+          Countdown Started for {value} minutes
+        </p>
+      )}
+      <div className="border-2 p-12 rounded-lg border-red-400 shadow-red-400 shadow-lg">
+        <p className="text-4xl tracking-widest font-extrabold">
+          <span>{timeLeft}</span> minutes left
+        </p>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <div className="mt-12 border-2 p-12 rounded-lg border-pink-400 shadow-pink-400 shadow-lg">
+        <div className="flex items-center gap-5">
+          <p className="text-2xl font-medium">Enter The time</p>
+          <Input
+            type="number"
+            className="w-fit"
+            size="lg"
+            value={counter.toString()}
+            placeholder="Ex: 10 minutes"
+            onChange={(e) => setCounter(Number(e.target.value))}
+            onKeyDown={handleEnter}
+          />
+          <Button
+            startContent={<LuTimerReset size={18} />}
+            onClick={handleReset}
+            variant="solid"
+          >
+            Reset
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    nextui({
+      prefix: "nextui",
+      addCommonColors: false,
+      defaultTheme: "dark",
+      defaultExtendTheme: "dark",
+    }),
+  ],
+};
