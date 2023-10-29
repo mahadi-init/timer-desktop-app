1fa645b52a9bbf66215cccad2c6cc8b0afacf119
Md. Nayeem <renayeem@gmail.com>
2023-10-30 00:23:21 +0600

    countdown timer design updated

--- src/components/CounterView.tsx
+++ src/components/CounterView.tsx
@@ -14,10 +14,10 @@ export default function CounterView({
           Countdown Started for {value} minutes
         </p>
       )}
-      <div className="border-2 p-12 rounded-lg border-red-400 shadow-red-400 shadow-lg">
-        <p className="text-4xl tracking-widest font-extrabold">
-          <span>{timeLeft}</span> minutes left
-        </p>
+
+      <div className="flex items-center justify-center gap-3 mt-10 p-3 rounded-full border-2 border-red-400 shadow-red-400 shadow-lg">
+        <p className="h-32 w-32 ps-1 flex items-center justify-center bg-orange-400 rounded-full shadow-orange-900 shadow-inner text-4xl tracking-widest font-extrabold">{timeLeft}</p>
+        <p className="text-4xl tracking-widest me-2 mb-1">minutes left</p>
       </div>
     </>
   );
