7178b3e4798cc4927142248badbac6bb967a0c45
mahadi-init <mahadi.init@proton.me>
2023-11-02 15:55:54 +0600

    update style and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,111 @@
-import { useDisclosure } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import {
   CounterView,
-  HistoryModal,
   InputGroup,
   ButtonGroup,
+  HistoryModal,
 } from "./components/get-components";
+import { useDisclosure } from "@nextui-org/react";
 
 function App() {
-  const [value, setValue] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   const [title, setTitle] = useState<string>();
-  const [counter, setCounter] = useState<number>(0);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   useEffect(() => {
-    if (timeLeft !== 0 && !isPaused) {
-      const timeout = setTimeout(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 60000);
+    let interval: any;
 
-      return () => clearTimeout(timeout);
+    if (isActive) {
+      interval = setInterval(() => {
+        if (minutes === 0 && seconds === 0) {
+          setIsActive(false);
+          clearInterval(interval);
+          console.log(minutes);
+        } else {
+          if (seconds === 0) {
+            setMinutes((prev) => prev - 1);
+            setSeconds(59);
+          } else {
+            setSeconds((prev) => prev - 1);
+          }
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval);
     }
-  }, [timeLeft, isPaused]);
 
+    return () => clearInterval(interval);
+  }, [isActive, minutes, seconds]);
 
-  const handleEnter = (e: any) => {
+  const handleStart = (e: any) => {
     if (e.key === "Enter") {
-      if (counter <= 0) return;
+      if (timer <= 0) return;
 
-      setValue(counter);
-      setTimeLeft(counter);
-      setCounter(0);
-
-      console.log(title);
-      //TODO: Add title in history
+      setMinutes(timer);
+      setSeconds(0);
+      setIsActive(true);
     }
+  };
+
+  const addMinutes = () => {
+    if (minutes == 0) return;
+    setMinutes((prev) => prev + 5);
+  };
+
+  const removeMinutes = () => {
+    if (minutes == 0) return;
+    if (minutes - 5 < 0) return 0;
+    setMinutes((prev) => prev - 5);
   };
 
   const handlePause = () => {
-    if (timeLeft === 0) return;
-
-    setIsPaused(!isPaused);
+    if (minutes == 0) return;
+    setIsActive(!isActive);
   };
 
   const handleReset = () => {
-    setTimeLeft(0);
-    setCounter(0);
-    setValue(0);
+    setIsActive(false);
+    setMinutes(0);
+    setSeconds(0);
   };
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <CounterView value={value} timeLeft={timeLeft} />
+      {timer === 0 ? (
+        <p className="font-medium mb-1">Countdown is not running</p>
+      ) : (
+        <p className="font-medium mb-1">
+          Countdown Started for {timer} minutes
+        </p>
+      )}
+      <CounterView minutes={minutes} seconds={seconds} />
       <div className="mt-12 border-2 p-12 rounded-lg border-pink-400 shadow-pink-400 shadow-lg">
         <div className="px-12 flex flex-col items-center gap-8">
           <InputGroup
             setTitle={setTitle}
-            setCounter={setCounter}
-            handleEnter={handleEnter}
+            setTimer={setTimer}
+            handleStart={handleStart}
+            isActive={isActive}
           />
           <ButtonGroup
-            isPaused={isPaused}
+            isActive={isActive}
+            addMinutes={addMinutes}
+            removeMinutes={removeMinutes}
             handlePause={handlePause}
             handleReset={handleReset}
           />
         </div>
       </div>
-      <HistoryModal
-        isOpen={isOpen}
-        onOpen={onOpen}
-        onOpenChange={onOpenChange}
-      />
+      <div className="mt-12">
+        <HistoryModal
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onOpenChange={onOpenChange}
+        />
+      </div>
     </div>
   );
 }
